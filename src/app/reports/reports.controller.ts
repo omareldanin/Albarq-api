8e80d71999d5d0a664ba159ca66d14be0a2451a0
omar mohamed <omer.eldanin@gmail.com>
2025-01-06 18:23:55 +0200

    fix reqports

--- src/app/reports/reports.controller.ts
+++ src/app/reports/reports.controller.ts
@@ -213,8 +213,13 @@ export class ReportController {
             reportsFilters: filters
         });
 
-        res.contentType("application/pdf");
-        res.send(pdf);
+        const pdfBuffer = Buffer.isBuffer(pdf) ? pdf : Buffer.from(pdf);
+        // Set headers for a PDF response
+        res.setHeader('Content-Type', 'application/pdf');
+        res.setHeader('Content-Disposition', 'attachment; filename=generated.pdf');
+        console.log('PDF size:', pdfBuffer.length);
+
+        res.send(pdfBuffer);
     });
 
     updateReport = catchAsync(async (req, res) => {
