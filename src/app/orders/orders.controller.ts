1dd8474943e1319c7b22600b687ffed3c5367951
omar mohamed <omer.eldanin@gmail.com>
2025-05-28 21:49:49 +0300

    add products

--- src/app/orders/orders.controller.ts
+++ src/app/orders/orders.controller.ts
@@ -284,6 +284,7 @@ export class OrdersController {
     };
     const loggedInUser = res.locals.user as loggedInUserType;
     const orderData = OrderUpdateSchema.parse(req.body);
+    console.log(req.body);
 
     const order = await ordersService.updateOrder({
       params: params,
