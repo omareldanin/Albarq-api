9fac8550253ebaf2c7a36c9c1710529b947eb96a
omar mohamed <omer.eldanin@gmail.com>
2025-06-09 09:04:14 +0300

    edit

--- src/server.ts
+++ src/server.ts
@@ -52,20 +52,20 @@ const server = newServer.listen(env.PORT, () => {
   automaticBackUpCronJob.start();
 });
 
-process.on("uncaughtException", (err) => {
-  // console.log("UNCAUGHT EXCEPTION! 💥 Shutting down...");
-  // console.log(err.name, "\n", err.message);
-  Logger.error("💥 UNCAUGHT EXCEPTION! 💥 Shutting down... 💥");
-  Logger.error(`${err.name}\n${err.message}`);
-  process.exit(1);
-});
+// process.on("uncaughtException", (err) => {
+//   // console.log("UNCAUGHT EXCEPTION! 💥 Shutting down...");
+//   // console.log(err.name, "\n", err.message);
+//   Logger.error("💥 UNCAUGHT EXCEPTION! 💥 Shutting down... 💥");
+//   Logger.error(`${err.name}\n${err.message}`);
+//   process.exit(1);
+// });
 
-process.on("unhandledRejection", (err: Error) => {
-  // console.log("UNHANDLED REJECTION! 💥 Shutting down...");
-  // console.log(err.name, err.message);
-  Logger.error("💥 UNHANDLED REJECTION! 💥 Shutting down... 💥");
-  Logger.error(`${err.name}\n${err.message}`);
-  server.close(() => {
-    process.exit(1);
-  });
-});
+// process.on("unhandledRejection", (err: Error) => {
+//   // console.log("UNHANDLED REJECTION! 💥 Shutting down...");
+//   // console.log(err.name, err.message);
+//   Logger.error("💥 UNHANDLED REJECTION! 💥 Shutting down... 💥");
+//   Logger.error(`${err.name}\n${err.message}`);
+//   server.close(() => {
+//     process.exit(1);
+//   });
+// });
