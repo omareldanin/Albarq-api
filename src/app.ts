cb5b356048ed05bd69a9ae7fddb497f61142d250
Omar Mohamed <omarmohamed@192.168.1.6>
2025-08-15 14:42:00 +0300

    s

--- src/app.ts
+++ src/app.ts
@@ -11,16 +11,17 @@ import morganBody from "morgan-body";
 // import { SwaggerTheme } from "swagger-themes";
 // import { rateLimit } from "express-rate-limit";
 import swaggerUi from "swagger-ui-express";
-import { AppError } from "./lib/AppError";
-import { Logger } from "./lib/logger";
+import {AppError} from "./lib/AppError";
+import {Logger} from "./lib/logger";
 import globalErrorHandler from "./middlewares/globalErrorHandler";
-import { morganMiddleware, morganMiddlewareImmediate } from "./middlewares/morgan";
+import {
+  morganMiddleware,
+  morganMiddlewareImmediate,
+} from "./middlewares/morgan";
 import apiRouter from "./routes";
 import swaggerDocument from "./swagger/swagger-output.json";
 
 const app = express();
-
-
 
 app.options("*", cors()); // include before other routes
 
@@ -28,79 +29,77 @@ app.use(cors());
 
 // Rate Limiting
 
-
 // const swaggerTheme = new SwaggerTheme("v3");
 const swaggerOptionsV1 = {
-    explorer: true
-    // customCss: swaggerTheme.getBuffer("dark"),
+  explorer: true,
+  // customCss: swaggerTheme.getBuffer("dark"),
 };
 
-app.use("/api-docs-dark-theme", swaggerUi.serve, swaggerUi.setup(swaggerDocument, swaggerOptionsV1));
-
-
+app.use(
+  "/api-docs-dark-theme",
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerDocument, swaggerOptionsV1)
+);
 
 app.use(bodyParser.json()); // Parse incoming request bodies in a middleware before your handlers, available under the req.body property.
-app.use(bodyParser.urlencoded({ extended: true })); // Parse incoming request bodies in a middleware before your handlers, available under the req.body property.
+app.use(bodyParser.urlencoded({extended: true})); // Parse incoming request bodies in a middleware before your handlers, available under the req.body property.
 app.use(cookieParser()); // Parse Cookie header and populate req.cookies with an object keyed by the cookie names.
 app.use(morganMiddlewareImmediate);
 morganBody(app, {
-    stream: {
-        // @ts-expect-error Fix later
-        write: (message) => Logger.info(message.replace(/\n$/, ""))
-    },
-    maxBodyLength: 200,
-    immediateReqLog: true,
-    // theme: "lightened",
-    noColors: true,
-    prettify: false
+  stream: {
+    // @ts-expect-error Fix later
+    write: (message) => Logger.info(message.replace(/\n$/, "")),
+  },
+  maxBodyLength: 200,
+  immediateReqLog: true,
+  // theme: "lightened",
+  noColors: true,
+  prettify: false,
 });
 app.use(morganMiddleware);
 app.use(
-    compression({
-        filter: (req, res) => {
-            if (req.headers["x-no-compression"]) {
-                return false;
-            }
-            return compression.filter(req, res);
-        },
-        threshold: 0
-    })
+  compression({
+    filter: (req, res) => {
+      if (req.headers["x-no-compression"]) {
+        return false;
+      }
+      return compression.filter(req, res);
+    },
+    threshold: 0,
+  })
 );
 
 app.use("/static", express.static("static"));
 app.use(
-    "/logs",
-    // isLoggedIn,
-    // isAutherized([Role.ADMIN]),
-    express.static("logs")
+  "/logs",
+  // isLoggedIn,
+  // isAutherized([Role.ADMIN]),
+  express.static("logs")
 );
 
 // Routes
 
-app.use(
-    "/api/v1",
-    apiRouter
-);
+app.use("/api/v1", apiRouter);
 
 app.route("/").get((_req, res) => {
-    // #swagger.ignore = true
-    res.send("<h1>Hello, World! 🌍 [From Root]</h1>");
+  // #swagger.ignore = true
+  res.send("<h1>Hello, World! 🌍 [From Root]</h1>");
 });
 
 app.route("/api").get((_req, res) => {
-    // #swagger.ignore = true
-    res.send("<h1>Hello, World! 🌍 [From API]</h1>");
+  // #swagger.ignore = true
+  res.send("<h1>Hello, World! 🌍 [From API]</h1>");
 });
 
 app.route("/health").get((_req, res) => {
-    // #swagger.ignore = true
-    res.sendStatus(200);
+  // #swagger.ignore = true
+  res.sendStatus(200);
 });
 
 app.all("*", (req, _res, next) => {
-    // #swagger.ignore = true
-    // Logger.error(`Can't find ${req.originalUrl} on this server!`);
-    next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+  // #swagger.ignore = true
+  // Logger.error(`Can't find ${req.originalUrl} on this server!`);
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
 
 // Global Error Handler
--- src/app/orders/orders.repository.ts
+++ src/app/orders/orders.repository.ts
@@ -486,12 +486,12 @@ export class OrdersRepository {
     let endDate = new Date();
     if (data.filters.startDate) {
       startDate = new Date(data.filters.startDate);
-      // startDate.setUTCDate(startDate.getUTCDate() - 1);
+      startDate.setHours(0, 0, 0, 0);
     }
     if (data.filters.endDate) {
       endDate = new Date(data.filters.endDate);
-      // endDate.setUTCDate(endDate.getUTCDate() + 1);
-      // endDate.setHours(23, 59, 59, 28);
+      endDate.setUTCDate(endDate.getUTCDate() + 1);
+      endDate.setHours(0, 0, 0, 0);
     }
     console.log("endDate", endDate);
 
@@ -1115,7 +1115,7 @@ export class OrdersRepository {
               {
                 createdAt: data.filters.startDate
                   ? {
-                      gt: startDate,
+                      gte: startDate,
                     }
                   : undefined,
               },
