34fec121c87ea69621702404f2e3358a3dde1c3f
Omar Mohamed <omarmohamed@192.168.1.14>
2025-11-09 14:35:35 +0200

    edit filters

--- src/app/orders/orders.repository.ts
+++ src/app/orders/orders.repository.ts
@@ -1515,6 +1515,7 @@ export class OrdersRepository {
                         {
                           client:
                             data.loggedInUser?.role !== "COMPANY_MANAGER" &&
+                            data.loggedInUser?.role !== "DELIVERY_AGENT" &&
                             !data.loggedInUser?.mainRepository
                               ? {
                                   branchId: data.loggedInUser?.branchId,
