3bdcba2021cd088e9d84a24626714f76ed204e20
Omar Mohamed <omer.eldanin@gmail.com>
2026-09-09 16:20:59 +0300

    edit

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,10 +3,15 @@ module.exports = {
     {
       name: "my-app",
       script: "build/server.js",
-      instances: 1, // 🔥 IMPORTANT for Puppeteer
+      instances: 1,
       exec_mode: "fork",
+
+      autorestart: true,
+      exp_backoff_restart_delay: 1000,
+
       max_memory_restart: "8000M",
       node_args: "--max-old-space-size=4024",
+
       env: {
         NODE_ENV: "production",
       },
