02f89b4aa9dcd04bf65aad0a52d39b6bd41bb6c6
Omar Mohamed <omer.eldanin@gmail.com>
2026-09-14 19:43:40 +0300

    edit

--- src/app/orders/helpers/generateReceipts.ts
+++ src/app/orders/helpers/generateReceipts.ts
@@ -7,18 +7,18 @@ import {Logger} from "../../../lib/logger";
 import {generatePDF} from "./generatePDF";
 
 export const generateReceipts = async (
-  orders: ReturnType<typeof orderReform>[]
+  orders: ReturnType<typeof orderReform>[],
 ) => {
   try {
     const templatePath = path.join(
       __dirname,
-      "../../../static/templates/receipt2.hbs"
+      "../../../static/templates/receipt4.hbs",
     );
 
     const template = await fs.readFile(templatePath, "utf8");
     const css = await fs.readFile(
       path.join(__dirname, "../../../static/styles/receiptStyle.css"),
-      "utf8"
+      "utf8",
     );
 
     const html = await generateHTML(template, {orders});
